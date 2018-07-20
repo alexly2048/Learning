1fcb5e2d208264a1ba2b2f9e7d63e65e7d5176ce
Felix.Li <314452354@qq.com>
2018-07-21 07:04:55 +0800

    Add c# dll example files

---
+++ C#/ExampleDemo/SortInterfaceExample/Square.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortInterfaceExample
+{
+    public class Square : IComparable<Square>
+    {
+        public Square() { }
+
+        public Square(int height,int width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Square square = obj as Square;
+            if (square != null)
+                return CompareTo(square);
+            throw
+                new ArgumentException("Both objects being compared must be of type Square.");
+        }
+
+        public override string ToString() => ($"Height :{this.Height}    Width: {this.Width}");
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Square square = obj as Square;
+            if (square != null)
+                return this.Height == square.Height;
+            return false;
+        }
+    }
+}
---
+++ C#/ExampleDemo/UnionStructExample/StructExample.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+
+namespace UnionStructExample
+{
+    /// <summary>
+    ///  通过预留字段来“补齐”内存分配
+    /// </summary>
+    public class StructExample
+    {
+        public byte b1;
+        public char c2;
+        public int i3;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public byte[] reserved;
+    }
+    /// <summary>
+    ///  MSDN   StructLayoutAttribute主题
+    /// </summary>
+    [StructLayoutAttribute(LayoutKind.Explicit)]
+    struct SignedNumber
+    {
+        [FieldOffsetAttribute(0)]
+        public sbyte Num1;
+        [FieldOffsetAttribute(0)]
+        public short Num2;
+        [FieldOffsetAttribute(0)]
+        public int Num3;
+        [FieldOffsetAttribute(0)]
+        public long Num4;
+        [FieldOffsetAttribute(0)]
+        public float Num5;
+        [FieldOffsetAttribute(0)]
+        public double Num6;
+    }
+
+    /// <summary>
+    ///  可以创建混合类型的联合结构体
+    ///  但不能把一种引用类型叠加到一种数值类型上
+    /// </summary>
+    [StructLayoutAttribute(LayoutKind.Explicit)]
+    struct SignedNumberWithText
+    {
+        [FieldOffsetAttribute(0)]
+        public sbyte Num1;
+        [FieldOffsetAttribute(0)]
+        public short Num2;
+        [FieldOffsetAttribute(0)]
+        public int Num3;
+        [FieldOffsetAttribute(0)]
+        public long Num4;
+        [FieldOffsetAttribute(0)]
+        public float Num5;
+        [FieldOffsetAttribute(0)]
+        public double Num6;
+        [FieldOffsetAttribute(16)]
+        public string Text1;
+    }
+
+}
