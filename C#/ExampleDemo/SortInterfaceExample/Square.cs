2a1b2ea67afd481e855af586d86515394090cbf7
Felix.Li <314452354@qq.com>
2018-07-22 08:04:14 +0800

    Add Sort Mehod Interface 'IComparable'

--- C#/ExampleDemo/SortInterfaceExample/Square.cs
+++ C#/ExampleDemo/SortInterfaceExample/Square.cs
@@ -18,6 +18,11 @@ namespace SortInterfaceExample
         public int Height { get; set; }
         public int Width { get; set; }
 
+        /// <summary>
+        ///  CompareTo方法重载
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public int CompareTo(object obj)
         {
             Square square = obj as Square;
@@ -29,6 +34,11 @@ namespace SortInterfaceExample
 
         public override string ToString() => ($"Height :{this.Height}    Width: {this.Width}");
 
+        /// <summary>
+        /// 重写Equals()方法
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
             if (obj == null)
@@ -37,6 +47,47 @@ namespace SortInterfaceExample
             if (square != null)
                 return this.Height == square.Height;
             return false;
+        }
+
+        /// <summary>
+        ///  重写GetHashCode()方法
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return this.Height.GetHashCode() | this.Width.GetHashCode();
+        }
+
+        /// <summary>
+        ///  重写运算符
+        ///   重写 == 方法
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static bool operator ==(Square x,Square y) => x.Equals(y);
+        public static bool operator !=(Square x,Square y)=> !(x==y);
+        public static bool operator <(Square x,Square y)=> (x.CompareTo(y) < 0);
+        public static bool operator >(Square x,Square y)=>(x.CompareTo(y)> 0);
+
+        /// <summary>
+        /// 调用IComparable,需实现CompareTo()方法
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(Square other)
+        {
+            long area1 = this.Height * this.Width;
+            long area2 = other.Height * other.Width;
+
+            if (area1 == area2)
+                return 0;
+            else if (area1 > area2)
+                return 1;
+            else if (area1 < area2)
+                return -1;
+            else
+                return -1;
         }
     }
 }
