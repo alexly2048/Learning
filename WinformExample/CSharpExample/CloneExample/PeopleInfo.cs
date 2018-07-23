38812d7ac141d9c9bdac58811e29e414c80135ef
Felix.Li <314452354@qq.com>
2018-07-23 09:19:33 +0800

    create 'Clone' example by felix.li at 2018.07.23

---
+++ WinformExample/CSharpExample/CloneExample/PeopleInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloneExample.Interface;
+
+namespace CloneExample
+{
+   public  class PeopleInfo : ShallowClone<PeopleInfo>
+    {
+        private string name = string.Empty;
+        private int age = 0;
+        public PeopleInfo Clone()
+        {
+            return this.Clone();
+        }
+#region Property
+        public string Name
+        {
+            get{ return this.name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Name不能为NULL或空值.");
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (age <= 0)
+                    throw new Exception("年龄必须大于零");
+                this.age = value;
+            }
+        }
+#endregion
+    }
+}
